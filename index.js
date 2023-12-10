d5f5f98322dfb3b05b3cb44fc70168cc0a79b7c6
villainishere <153314738+villainishere@users.noreply.github.com>
2023-12-10 23:11:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 
-say('Nao-Botz MD', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
-say(`'Whatsapp Bot MD by Shirokami Ryzen`, { font: 'console', align: 'center', gradient: ['blue', 'green'] })
+say('VillainBot', { font: 'chrome', align: 'center', gradient: ['magenta', 'yellow'] })
+say(`'Whatsapp Bot MD by Gu Changge`, { font: 'console', align: 'center', gradient: ['magenta', 'black'] })
 
 var isRunning = false
 /**
